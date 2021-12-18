646d51a82d1594315fcfeb6b890deb234ed7a9db
Sam <17427046+Samillion@users.noreply.github.com>
2021-12-18 09:07:49 +0200

    Update skyforge-FR.go

--- translations/skyforge-FR.go
+++ translations/skyforge-FR.go
@@ -12,47 +12,47 @@
 {{if eq $skyArg "armes"}}
 {{$embed := cembed 
 "title" "Recommandations pour les armes"
-"description" "Les armes listées ci-dessous sont considérées (selon les joueurs) comme prioritaires, essentielles.\n\nC'est pourquoi toutes les classes ne sont pas listées. Réfléchissez toujours à quelle arme vous voulez en premier.\n\n**Lumancien:**\n- Baguette de Telurgan (Support, PvP)\n\n**Décibel:**\n- Khella (Support, Comp, PvP)\n\n**Saronide:**\n- Sigil (DPS, Tank)\n- Arme d'endurance (Tank)\n\n**Paladin:**\n- Chagrin et Lamentation (Tank, DPS, PvP)\n\n**Nécromancien:**\n- Morana (DPS)\n- Morodea (DPS, PvP)\n\n**Berserker:**\n- Coeur de Fer (DPS, PvP)\n\n**Revenant:**\n- Baron (DPS)\n- Ner'gal (PvP)"
+"description" "Les armes listées ci-dessous sont considérées selon les joueurs comme prioritaires.\n\nC'est pourquoi toutes les classes ne sont pas listées. Réfléchissez toujours à quelle arme vous voulez en premier\n\n**Lumancien:**\n- Telurgan (Support, PvP)\n\n**Décibel:**\n- Khella (Support, Comp, PvP)\n\n**Saronide:**\n- Sigil (DPS, Tank)\n- Arme d'endurance (Tank)\n\n**Paladin:**\n- Chagrin et Lamentation (Tank, DPS, PvP)\n\n**Nécromancien:**\n- Morana (DPS)\n- Morodea (DPS, PvP)\n\n**Berserker:**\n- Coeur de Fer (DPS, PvP)\n\n**Revenant:**\n- Baron (DPS)\n- Ner'gal (PvP)"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "ether"}}
 {{$embed := cembed 
 "title" "Éther (Terra)"
-"description" "L'Éther est la ressource utilisée pour débloquer vos Symboles de classe sur Terra (Écarlate, Or, Émeraude). Pour débloquer la mission d'un symbole de terra, vous avez besoin de 1000 d'Éther.\n\n**Source d'Éther** (Fleur d'Éther)\n- Terra (Région)\n- Coeur de la Ville (Groupe)\n- Terres Gastes Toxiques (Groupe)\n- Pistes Anciennes (Groupe)\n- Faille Terranne (Unité)"
+"description" "L'Éther est la ressource utilisée pour débloquer vos Symboles de classe sur Terra (Écarlate, Or, Émeraude). Pour débloquer la mission d'un symbole, vous avez besoin de 1000 d'Éther.\n\n**Source d'Éther** (Fleur d'Éther)\n- Terra\n- Coeur de la Ville\n- Terres Gastes Toxiques\n- Pistes Anciennes\n- Faille Terranne"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "adeptes"}}
 {{$embed := cembed 
 "title" "Adeptes (Foi)"
-"description" "- Ne jamais annuler un voyage pour récupérer les récompenses.\n- Ajouter des reliques avec du Charisme & de la Constitution pour récupérer plus de foi.\n- Toujours envoyer ses adeptes en voyage.\n- Seulement 8 adeptes sont autorisés.\n- Toujours garder les adeptes avec les stats les plus hautes.\n\n**Notes:**\n- Avoir plus de 8 adeptes réduit la foi gagnée de 50%.\n- Lors des derniers 25% de son voyage, un adepte ramène plus de foi.\n- Plus vous gardez vos adeptes longtemps, plus leurs stats vont évoluer."
+"description" "- Ne jamais annuler un voyage en cours\n- Ajouter des reliques avec du Charisme & de la Constitution pour avoir plus de foi\n- Toujours envoyer ses adeptes en voyage\n- Seuls 8 adeptes sont autorisés\n- Toujours garder les adeptes avec les stats les plus hautes\n\n**Notes:**\n- Avoir plus de 8 adeptes réduit la foi gagnée de 50%\n- Lors des derniers 30% de son voyage, un adepte ramène plus de foi\n- Plus vous gardez vos adeptes longtemps, plus leurs stats vont évoluer"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "astuces"}}
 {{$embed := cembed 
-"title" "Petites Astuces"
-"description" "**Quelle classe débloquer en premier?**\nLes joueurs recommandent le Saronide, le Feurioso ou le Revenant pendant que vous gagnez en rang, déblocables avec les Étincelles de Transformation.\n\nCes classes vous permettent de survivre tout en infligeant de bons dégâts.\n\n**Cathédrale et Tour de la Connaissane:**\nLes joueurs vous recommandent généralement de ne pas gaspiller vos crédits pendant que vous montez en niveau ; vous aurez besoin de ces crédits pour d'autres choses (ie: l'équipement) et vous allez obtenir des fidèles pour améliorer le rang de votre Cathédrale en récompense.\n\nAugmentez votre Tour de la Connaissance avec les ressources que vous avez, mais n'utilisez pas beaucoup de Stimulants..\n\n**Aelerium:**\nIl s'agit de la ressource utilisée pour débloquer les talents Aelerium-9 (A9). Ils peuvent être récupérés à Théa (Région), en tuant Kairax ou en participant aux évènements de la région.\n\n**Quel bonus débloquer en priorité dans la salle des trophées:**\n- Médecin Personnel\n- Ramener le maître à la vie (et Théa-1)\n- Chasseur de trésors\n- Glyphe d'Asterius (KoE Farm)\n\n**Important:** Il est primordial d'avoir toujours l'esprit d'équipe, d'être bon joueur et de s'adapter aux besoins du groupe."
+"title" "Astuces"
+"description" "**Quelle classe débloquer en premier?**\nLes joueurs recommandent le Saronide, le Feurioso ou le Revenant pendant que vous gagnez en rang ; ces classes octroient de la survibabilité et des dégâts\n\n**Cathédrale et Tour de la Connaissane:**\nLes joueurs vous recommandent de ne pas gaspiller vos crédits ; vous en aurez besoin pour d'autres choses (ie: l'équipement) et vous obtiendrez des fidèles en récompenses de directives pour améliorer votre Cathédrale. Augmentez votre Tour de la Connaissance quand vous le pouvez, mais n'utilisez pas de Stimulants.\n\n**Aelerium:**\nIl s'agit de la ressource utilisée pour débloquer les talents Aelerium-9 (A9). Ils peuvent êtres récupérés à Théa (région), en tuant Kairax ou en participant aux évènements.\n\n**Priorités dans la Salle des Trophées:**\n- Médecin Personnel\n- Ramener le maître à la vie (et Théa-1)\n- Chasseur de trésors\n- Glyphe d'Asterius (KoE Farm)\n\n**Important:** Il est primordial d'avoir l'esprit d'équipe, d'être bon joueur et de s'adapter aux besoins du groupe"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "aspects"}}
 {{$embed := cembed 
 "title" "Formes d'Aspects"
-"description" "Aspects: Ténacité, Rage, Pitié, Magie, Vengeance.\n\nAu début, vous avez 3 aspects disponibles (Ténacité, Rage, Pitié). Pour débloquer les deux autres aspects, vous devez vaincre deux Avatars Champions.\n\n**Priorité des Aspects:**\nChaque aspect prend du temps ; pour terminer les 5 aspects, vous avez besoin d'un total de 3.1 million de Cognition.\n\nPour cette raison, les joueurs recommandent de compléter vos aspects dans cet ordre:\n\n**Premier Aspect : la Pitié**\nCeci est le point sur lequel tout le monde s'entend concernant les aspects : complétez tout d'abord la Pitié. Le nodule d'or apporte d'énormes avantages, dont les élixirs, qui vous permettent d'utiliser gratuitement votre forme d'Aspect.\n\n**Après la Pitié:**\nIl y a beaucoup de débats sur l'aspect à prioriser après la Pitié, cela va en grande partie dépendre de votre gameplay. On vous recommande fortement de vérifier chaque nodule d'or pour vous aider à vous décider."
+"description" "Aspects: Ténacité, Rage, Pitié, Magie, Vengeance.\n\nAu début, vous avez 3 aspects disponibles (Ténacité, Rage, Pitié). Pour débloquer les deux autres, vous devez tuer deux Avatars Champions.\n\n**Priorité des Aspects:**\nChaque aspect est long à monter ; pour tous les terminer, vous avez besoin de 3,1 million de Cognition. C'est pour ça que les joueurs recommandent de compléter vos aspects dans cet ordre:\n\n**Premier Aspect : la Pitié**\nTous les joueurs sont d'accord là-dessus. Son nodule d'or apporte d'énormes avantages, dont les élixirs qui remboursent transformation en Aspect.\n\n**Après la Pitié:**\nIl y a beaucoup de débats sur quel aspect monter ensuite, cela dépendra en grande partie de votre gameplay. On vous recommande de vérifier chaque nodule d'or pour vous aider à vous décider."
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "cog"}}
 {{$embed := cembed 
 "title" "Cognition"
-"description" "La Cognition est la ressource utilisée pour développer vos Aspects.\n\n**Comment gagner de la Cognition:**\n- En récompense de directive.\n- En terminant l'Avatar d'entraînement, l'Avatar Champion et les distortions.\n- En tuant les boss des régions envahies (évènemement).\n\n**Notes:**\n- En terminant chaque Aspect (Nodule d'or), vous gagnerez plus de Cognition.\n- En activant le Sceau des Exploits dans votre Cathédrale, vous gagnerez plus de Cognition en tuant les boss des régions envahies."
+"description" "La Cognition est la ressource utilisée pour développer vos Aspects\n\n**Comment gagner de la Cognition:**\n- Sur vos directives.\n- En terminant l'Avatar d'entraînement, l'Avatar Champion et les distortions\n- En tuant les boss des régions envahies (évènemement)\n\n**Notes:**\n- En complétant chaque Aspect, vous gagnerez plus de Cognition\n- Activez le Sceau des Exploits dans votre Cathédrale pour gagner plus de Cognition en tuant les boss des régions envahies"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "argents"}}
 {{$embed := cembed 
 "title" "Argents"
-"description" "Les Argents sont la monnaie payante utilisée pour acheter des armes légendaires, des tenues, des montures et autres\n\n**Comment obtenir des Argents:**\n- Se positionner dans le top 50% des opérations quotidiennes (se réinitialisent toutes les 2 semaines)\n- En terminant la directive de l'Avatar d'Entraînement.\n- En participant aux Inghar.\n- En l'achetant avec de la monnaie réelle."
+"description" "Les Argents sont la monnaie payante utilisée pour acheter des armes légendaires, des tenues, des montures, etc\n\n**Comment en obtenir:**\n- Se positionner dans le top 50% des opérations quotidiennes (se réinitialisent toutes les 2 semaines)\n- Terminer la directive de l'Avatar d'Entraînement\n- Participer aux Inghar\n- En acheter avec de la monnaie réelle"
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $skyArg "build"}}
-{{$allowedRoles := (cslice "dps" "support" "tank" "companion" "pvp")}}
+{{$allowedRoles := (cslice "dps" "support" "tank" "companion" "comp" "pvp")}}
 {{if eq (len .Args) 2}}
 {{$embed := cembed 
 "title" "Sub-options"
@@ -65,36 +65,36 @@
 {{if in $allowedRoles $role}}
 {{if eq $role "dps"}}
 {{$embed := cembed 
-"title" "DPS Build"
-"description" "DPS build by **Leo Scrame**\n\n**Note:**\nThe first two symbol sets below are based on protection, whether you want it to save you from a one shot or have protection from DoT.\n\nThe last symbol set is effective against mobs.\n\n**Recommended Artifact:**\n- Black Flame\n- Vulnerability Detector\n\n**Adept Relics:**\nIt is recommended to balance Main Damage and Critical Damage relics or go with Main Damage ones.\n\n**Recommended Divine Weapon:**\n- Nerion's Hammer\n- Cyrus's Blades\n- Integrator's Transformer"
+"title" "Build DPS"
+"description" "Build proposé par Leo Scrame\n\n**Note:**\nLes deux premiers sets de symboles montrés sous le build sont basés sur de la protection contre les 'one-shots' (1er) ou contre des dots (2ieme). Le dernier set est efficace contre les monstres.\n\n**Artefacts:**\n- Flamme Noire\n- Détecteur de Vulnérabilité\n\n**Reliques:**\nIl est recommandé d'équilibrer les reliques avec des Dégâts Principaux et des Dégâts Critiques, ou de mettre uniquement des Dégâts Principaux.\n\n**Armes Divines:**\n- Marteau de Nerion\n- Lames de Cyrus\n- Intégrateur"
 "image" (sdict "url" "https://media.discordapp.net/attachments/913303283110649916/920994016303120394/full-dps-build-fr.png")
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $role "support"}}
 {{ $embed := cembed 
-"title" "Support Build"
-"description" "Support Build by **Sam Sinner**, **Leo Scrame** and **Laama Laamanen**.\n\n**Note:**\nIgnore the Honor stat in the gear.\n\n**Recommended Artifact:**\n- Wings of Skies (Might buffs)\n- Overload Module (with Support Mode) (Optional)\n\n**Recommended Divine Weapon:**\n- Machavann's Guard (Might buff)\n- Herida's Wings"
+"title" "Build Support"
+"description" "Build proposé par Sam Sinner, Leo Scrame et Laama Laamanen.\n\n**Note:**\nVous pouvez ignorer la statistique d'Honneur de cet équipement.\n\n**Artefacts:**\n- Ailes des cieux (buffs de puissance)\n- Module Surcharge (avec le Mode Soutien) (Optionnel)\n\n**Armes Divines:**\n- Machavann\n- Ailes d'Herida"
 "image" (sdict "url" "https://media.discordapp.net/attachments/913303283110649916/920994378300923955/full-support-build-fr.png")
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $role "tank"}}
 {{$embed := cembed 
-"title" "Tank Build"
-"description" "Tank build by **Leo Scrame**.\n\n**Note:**\nMany Artifacts work well with a Tank build, so we decided to list all the useful ones in order.\n\n**Recommended Artifact:**\n- Aegis of the Ocean\n- Overload Module (with Implacability)\n- Viper Wristlet\n- Impenetrable Shield\n\n**Recommended Divine Weapon:**\n- Protheus's Trident\n- Laertes's Sword"
+"title" "Build Tank"
+"description" "Build proposé par Leo Scrame\n\n**Note:**\nBeaucoup d'artefacts sont utiles pour tanker, donc nous avons décidé de lister les plus utiles.\n\n**Artefacts:**\n- Égide de l'Océan\n- Module Surcharge (avec Implacabilité)\n- Bracelet de Vipère\n- Bouclier impénétrable\n\n**Armes Divines:**\n- Trident de Prothéus\n- Épée de Laërte"
 "image" (sdict "url" "https://media.discordapp.net/attachments/913303283110649916/920994613840465920/full-tank-build-fr.png")
 "color" $colorGold}}
 {{sendMessage nil $embed}}
-{{else if eq $role "companion"}}
+{{else if or (eq $role "companion") (eq $role "comp")}}
 {{$embed := cembed 
-"title" "Companion Build"
-"description" "Companion build by **Sam Sinner**, **Leo Scrame** and **Zanzuro Mizoru**.\n\n**Note:**\nThis build is perfect with Soundweaver. However, you can also use it with other classes such as Lightbinder.\n\nYou can also replace the Bracelet with Zeal.\n\n**Recommended Artifact:**\n- Overload Module\n- Hyper Engine (Soundweaver)\n\n**Recommended Divine Weapon:**\n- Cyrus's Blades"
+"title" "Build Compagnon"
+"description" "Build proposé par Sam Sinner, Leo Scrame et Zanzuro Mizoru.\n\n**Note:**\nCe build marche bien avec le Décibel. Mais vous pouvez l'utiliser avec d'autres classes\n\nVous pouvez aussi remplacer le Bracelet avec le Zèle.\n\n**Artefacts:**\n- Module Surcharge\n- Moteur Hyper (Décibel)\n\n**Arme Divine:**\n- Lames de Cyrus"
 "image" (sdict "url" "https://media.discordapp.net/attachments/913303283110649916/920993893217099806/full-companion-build-fr.png")
 "color" $colorGold}}
 {{sendMessage nil $embed}}
 {{else if eq $role "pvp"}}
 {{$embed := cembed 
-"title" "PvP Build"
-"description" "PvP build by **Zanzuro Mizoru**\n\n**Note:**\nFor additional defense, you can replace the Sapphire with Beacon or to decrease opponents defense, replace Ruby with Emitter.\n\n**Recommended Artifact:**\n- Glyph of Despair\n- Viper Wristlet\n- Trewang's Idol\n\n**Adept Relics:**\nMany PvP players recommend to use Defense relics."
+"title" "Build PVP"
+"description" "Build proposé par Zanzuro Mizoru\n\n**Note:**\nPour avoir plus de défense, vous pouvez remplacer le Saphir avec la Balise de Protection. Pour diminuer la défense de vos ennemis, remplacez le Rubis avec l'Emetteur du Désespoir\n\n**Artefacts:**\n- Glyphe du Désespoir\n- Bracelet de Vipère\n- Trewang\n\n**Reliques:**\nBeaucoup de joueurs PvP utilisent des reliques de Défense."
 "image" (sdict "url" "https://media.discordapp.net/attachments/913303283110649916/920994248160071700/full-pvp-build-fr.png")
 "color" $colorGold}}
 {{sendMessage nil $embed}}
